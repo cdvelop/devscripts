feac218459593d9b85ba29cab0dd02520bb86b40
César Solís <Csar Sols@users.noreply.github.com>
2025-07-03 16:34:16 -0400

    Rename file from backupwindows.sh to autoBackup.sh

---
+++ autoBackup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Performs backup operations using FreeFileSync on Windows systems
+# Usage: ./autoBackup.sh
+
+source functions.sh
+
+backup() {
+  case "$OSTYPE" in
+    msys*|mingw*)
+      warning "FreeFileSync backup started...."
+      (execute '"/c/Program Files/FreeFileSync/FreeFileSync.exe" "/c/Users/$(whoami)/SyncWin/SyncSettings.ffs_batch"' &) >/dev/null 2>&1
+      # Adding & at the end of the command will run it in background and free up the terminal
+      ;;
+    *)
+      error "This operating system '$OSTYPE' is not compatible with the backup function."
+      ;;
+  esac
+}
+
+backup
