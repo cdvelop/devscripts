99ec87ad74fde308f5d08281cc3c7147b9bbd038
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2025-08-14 21:08:14 -0400

    disabla auto update

--- gomodcheck.sh
+++ gomodcheck.sh
@@ -10,7 +10,7 @@ if [ -f "go.mod" ]; then
   go_mod_name=$(gawk -v pattern=$currentGitHostUserPath/ 'NR==1 && match($0, pattern "([^/]+)", arr) { print arr[1] }' go.mod)
   
   # Update current packages
-  execute "go get -u all" "failed to update packages in $go_mod_name" 
+  # execute "go get -u all" "failed to update packages in $go_mod_name" 
   
   # Verify code
   execute "go mod tidy" "go mod tidy failed in $go_mod_name" "go mod tidy $go_mod_name ok"
